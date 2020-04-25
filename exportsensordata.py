01da034e809b6c52bb47217b9a0180c32598eed5
Steve <steve.clifton@outlook.com>
2020-04-26 11:22:08 +1200

    Add env for testing

--- exportsensordata.py
+++ exportsensordata.py
@@ -1,11 +1,12 @@
 from database import getDatabase
 from export import exportSensorData
+import env
 
 # start program
 db = getDatabase()
 
 # Change this to True if you want to test inserting random data to try export
-if False:
+if env.LIVE_TEST_DATA:
 	db.execute("INSERT INTO bme280 (temperature, humidity, pressure) VALUES(23,33,44)")
 
 exportSensorData(db)
