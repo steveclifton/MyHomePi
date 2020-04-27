920e563ea5b5dcd7375f25ab85560361a04e86df
Steve <steve.clifton@outlook.com>
2020-04-27 14:56:46 +1200

    update .env.example

--- env.example.py
+++ env.example.py
@@ -2,13 +2,21 @@ APP_LIVE = False
 
 DEVICE = ''
 
+LIVE_BME280_DEVICES = {
+	'76': 'Indoor'
+}
+
+BUILD_BME280_DEVICES = {
+	'76': 'Indoor'
+}
+
 LIVE_HTTP_URI = ''
-BUILD_HTTP_URI = ''
+BUILD_HTTP_URI = 'http://localhost:8000/api/home/reading'
 
 LIVE_TOKEN = ''
 BUILD_TOKEN = ''
 
 LIVE_DATABASE = ''
-BUILD_DATABASE = ''
+BUILD_DATABASE = 'database.sqlite'
 
 LIVE_TEST_DATA = True
