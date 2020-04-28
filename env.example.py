89486ae595eaa41051629a9b715c1e14d9a2f17c
Steve <steve.clifton@outlook.com>
2020-04-28 21:27:36 +1200

    Token update

--- env.example.py
+++ env.example.py
@@ -10,7 +10,7 @@ LIVE_HTTP_URI = ''
 BUILD_HTTP_URI = 'http://localhost:8000/api/home/reading'
 
 LIVE_TOKEN = ''
-BUILD_TOKEN = 'bnma984mcjak'
+BUILD_TOKEN = 'eyJ0eXAiOifKV1QiLCJhbGcaOiJSUzI1NiJ9.......'
 
 LIVE_DATABASE = ''
 BUILD_DATABASE = 'database.sqlite'
