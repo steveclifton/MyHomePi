d67dd3f412387ebb926a777db3836a4f47a966e0
Steve <steve.clifton@outlook.com>
2020-04-30 21:08:08 +1200

    fix indent

--- database.py
+++ database.py
@@ -14,7 +14,7 @@ class Database:
 
 	def getDatabase():
 		if env.APP_LIVE:
-		return sqlite3.connect(env.LIVE_DATABASE)
+			return sqlite3.connect(env.LIVE_DATABASE)
 	else:
 		return sqlite3.connect(env.BUILD_DATABASE)
 
