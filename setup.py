83f0ba334d581fa210b199ffb09e2a1acd42f3b2
Steve <steve.clifton@outlook.com>
2020-05-01 19:59:12 +1200

    Update

--- setup.py
+++ setup.py
@@ -5,27 +5,18 @@ import database
 db = database.getDatabase()
 
 try:
-    db.execute('''CREATE TABLE IF NOT EXISTS bme280
-           (ID INTEGER PRIMARY KEY AUTOINCREMENT,
-           temperature REAL NOT NULL,
-           humidity REAL NOT NULL,
-           pressure REAL NOT NULL,
-           created TIMESTAMP NOT NULL,
-           exported INT DEFAULT 0);''')
 
-    db.execute('''CREATE TABLE IF NOT EXISTS errorlogs
+    # Migration to add reading table
+    db.execute('''
+          CREATE TABLE IF NOT EXISTS readings
            (ID INTEGER PRIMARY KEY AUTOINCREMENT,
-           log TEXT NOT NULL,
+           key TEXT NOT NULL,
+           value REAL NOT NULL,
+           deviceid INTEGER NOT NULL DEFAULT 0,
            created TIMESTAMP NOT NULL,
-           exported INT DEFAULT 0);''')
+           exported INT DEFAULT 0);
+           ''')
 
-    # Migration to add deviceid
-    db.execute('''ALTER TABLE bme280
-      ADD deviceid INTEGER NOT NULL DEFAULT 0
-      ''')
-    db.execute('''ALTER TABLE errorlogs
-      ADD deviceid INTEGER NOT NULL DEFAULT 0
-      ''')
     # End Migration
 
 except Exception as e:
